1efa5c06fdcc4239f6c7f083d60f4a5d3c756fe6
Anthony Nixon <anixon604@gmail.com>
2020-06-09 17:17:43 +0100

    supplychain: removed deprecated function from MockNetwork

--- Accounts/supplychain/workflows/src/test/kotlin/com/accounts_SupplyChain/FlowTests.kt
+++ Accounts/supplychain/workflows/src/test/kotlin/com/accounts_SupplyChain/FlowTests.kt
@@ -1,12 +1,18 @@
 package com.accounts_SupplyChain
 
 import net.corda.testing.node.MockNetwork
+import net.corda.testing.node.MockNetworkParameters
+import net.corda.testing.node.internal.findCordapp
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
 
 class FlowTests {
-    private val network = MockNetwork(listOf("com.accounts_SupplyChain"))
+    private val network = MockNetwork(MockNetworkParameters().withCordappsForAllNodes(
+            listOf(
+                    findCordapp("com.accounts_SupplyChain.contracts")
+            )
+    ))
     private val a = network.createNode()
     private val b = network.createNode()
 
