c7d1d057c226cf0e75541f3bc16aa192a37ae507
peterli-r3 <51169685+peterli-r3@users.noreply.github.com>
2020-07-20 11:55:43 -0400

    move token upgrade

--- Tokens/bikemarket/workflows/src/main/kotlin/net/corda/examples/bikemarket/flows/TransferBikeToken.kt
+++ Tokens/bikemarket/workflows/src/main/kotlin/net/corda/examples/bikemarket/flows/TransferBikeToken.kt
@@ -2,12 +2,17 @@ package net.corda.examples.bikemarket.flows
 
 import co.paralleluniverse.fibers.Suspendable
 import com.r3.corda.lib.tokens.contracts.types.TokenPointer
-import com.r3.corda.lib.tokens.workflows.flows.rpc.MoveNonFungibleTokens
-import com.r3.corda.lib.tokens.workflows.flows.rpc.MoveNonFungibleTokensHandler
-import com.r3.corda.lib.tokens.workflows.types.PartyAndToken
+import com.r3.corda.lib.tokens.workflows.flows.move.addMoveNonFungibleTokens
+import com.r3.corda.lib.tokens.workflows.internal.flows.distribution.UpdateDistributionListFlow
+import com.r3.corda.lib.tokens.workflows.internal.flows.finality.ObserverAwareFinalityFlow
+import com.r3.corda.lib.tokens.workflows.internal.flows.finality.ObserverAwareFinalityFlowHandler
+import com.r3.corda.lib.tokens.workflows.utilities.getPreferredNotary
+import net.corda.core.contracts.requireThat
 import net.corda.core.flows.*
 import net.corda.core.identity.Party
 import net.corda.core.node.services.queryBy
+import net.corda.core.transactions.SignedTransaction
+import net.corda.core.transactions.TransactionBuilder
 import net.corda.core.utilities.ProgressTracker
 import net.corda.examples.bikemarket.states.FrameTokenState
 import net.corda.examples.bikemarket.states.WheelsTokenState
@@ -34,9 +39,6 @@ class TransferBikeToken(val frameSerial: String,
 
         //get the pointer pointer to the frame
         val frametokenPointer: TokenPointer<*> = frametokentype.toPointer(frametokentype.javaClass)
-        val partyAndFrameToken = PartyAndToken(holder,frametokenPointer)
-
-        val stx1 = subFlow(MoveNonFungibleTokens(partyAndFrameToken))
 
         //Step 2: Wheels Token
         val wheelStateAndRef = serviceHub.vaultService.queryBy<WheelsTokenState>().states
@@ -47,22 +49,28 @@ class TransferBikeToken(val frameSerial: String,
 
         //get the pointer pointer to the wheel
         val wheeltokenPointer: TokenPointer<*> = wheeltokentype.toPointer(wheeltokentype.javaClass)
-        val partyAndWheelToken = PartyAndToken(holder, wheeltokenPointer)
 
-        val stx2 = subFlow(MoveNonFungibleTokens(partyAndWheelToken))
+        //send tokens
+        val session = initiateFlow(holder)
+        val txBuilder = TransactionBuilder(getPreferredNotary(serviceHub))
+        addMoveNonFungibleTokens(txBuilder,serviceHub,frametokenPointer,holder)
+        addMoveNonFungibleTokens(txBuilder,serviceHub,wheeltokenPointer,holder)
+        val ptx = serviceHub.signInitialTransaction(txBuilder)
+        val stx = subFlow(CollectSignaturesFlow(ptx, listOf(session)))
+        subFlow(UpdateDistributionListFlow(stx))
+        val ftx = subFlow(ObserverAwareFinalityFlow(stx, listOf(session)))
 
         return ("\nTransfer ownership of a bike (Frame serial#: " + this.frameSerial + ", Wheels serial#: " + this.wheelSerial + ") to "
                 + holder.name.organisation + "\nTransaction IDs: "
-                + stx1.id + ", " + stx2.id)
+                + ftx.id)
     }
 }
 
 @InitiatedBy(TransferBikeToken::class)
-class TransferBikeTokenResponder(val counterpartySession: FlowSession) : FlowLogic<Unit>() {
+class TransferBikeTokenResponder(val flowSession: FlowSession) : FlowLogic<Unit>() {
     @Suspendable
-    override fun call() {
-        // Responder flow logic goes here.
-        subFlow(MoveNonFungibleTokensHandler(counterpartySession));
+    override fun call(): Unit {
+        subFlow(ObserverAwareFinalityFlowHandler(flowSession))
     }
 }
 
