9be6e8cfbcb3028930ea92a295f386f09eae7fe1
david awad <me@davidawad.com>
2020-12-22 13:09:53 -0500

    fixing kotlin web client issue

--- Advanced/snakesandladders-cordapp/clients/src/main/kotlin/net/corda/samples/snl/webserver/Server.kt
+++ Advanced/snakesandladders-cordapp/clients/src/main/kotlin/net/corda/samples/snl/webserver/Server.kt
@@ -3,29 +3,20 @@ package net.corda.samples.snl.webserver
 import org.springframework.boot.Banner
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.WebApplicationType.SERVLET
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.context.annotation.ComponentScan
 
 /**
  * Our Spring Boot application.
  */
-@EnableAutoConfiguration
-@ComponentScan
 @SpringBootApplication
-open class Server{
-    companion object {
+private open class Server
 
-        /**
-         * Starts our Spring Boot application.
-         */
-        @JvmStatic
-        fun main(args: Array<String>) {
-            val app = SpringApplication(Server::class.java)
-            app.setBannerMode(Banner.Mode.OFF)
-            app.webApplicationType = SERVLET
-            app.run(*args)
-        }
-    }
+/**
+ * Starts our Spring Boot application.
+ */
+fun main(args: Array<String>) {
+    val app = SpringApplication(Server::class.java)
+    app.setBannerMode(Banner.Mode.OFF)
+    app.webApplicationType = SERVLET
+    app.run(*args)
 }
-
