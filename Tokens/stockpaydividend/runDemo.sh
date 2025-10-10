551dc6a3855254a35fca80afb69bbc37dce5e37a
Szymon Sztuka <szymon.sztuka@r3.com>
2025-10-10 11:50:45 +0100

    Small fixes

--- Tokens/stockpaydividend/runDemo.sh
+++ Tokens/stockpaydividend/runDemo.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-java -jar build/libs/admin-cli-4.14-SNAPSHOT.jar create-network -u http://localhost:8899 -v
-java -jar build/libs/admin-cli-4.14-SNAPSHOT.jar authorize --address Dev7chG99tLCAny3PNYmBdyhaKEVcZnSTp3p1mKVb5m5 --network 1 -u http://localhost:8899
-java -jar build/libs/admin-cli-4.14-SNAPSHOT.jar list-notaries -u http://localhost:8899
+ADMIN_CLI="solana-aggregator/admin-cli/build/libs/admin-cli-4.14-SNAPSHOT.jar"
+
+java -jar $ADMIN_CLI create-network -u http://localhost:8899 -v -k solana-aggregator/notary-program/dev-keys/DevAD5S5AFhTTCmrD8Jg58bDhbZabSzth7Bu6rG4HFYo.json
+java -jar $ADMIN_CLI authorize --address Dev7chG99tLCAny3PNYmBdyhaKEVcZnSTp3p1mKVb5m5 --network 1 -u http://localhost:8899 -k solana-aggregator/notary-program/dev-keys/DevAD5S5AFhTTCmrD8Jg58bDhbZabSzth7Bu6rG4HFYo.json
+java -jar $ADMIN_CLI list-notaries -u http://localhost:8899 -v -k solana-aggregator/notary-program/dev-keys/DevAD5S5AFhTTCmrD8Jg58bDhbZabSzth7Bu6rG4HFYo.json
 
 solana config set --url localhost
 
--- Tokens/stockpaydividend/runSolana.sh
+++ Tokens/stockpaydividend/runSolana.sh
@@ -5,13 +5,14 @@
 cd solana-aggregator/notary-program
 anchor build
 
+
 #start
 solana-test-validator --reset --ledger ../admin-cli/build/test-ledger --bpf-program target/deploy/corda_notary-keypair.json target/deploy/corda_notary.so
 
 #new terminal
-ADMIN_CLI="./solana-aggregator/admin-cli/build/libs/admin-cli-4.14-SNAPSHOT.jar"
+ADMIN_CLI="solana-aggregator/admin-cli/build/libs/admin-cli-4.14-SNAPSHOT.jar"
 solana airdrop -k solana-aggregator/notary-program/dev-keys/DevAD5S5AFhTTCmrD8Jg58bDhbZabSzth7Bu6rG4HFYo.json --commitment confirmed 10
 solana airdrop -k solana-aggregator/notary-program/dev-keys/DevNMdtQW3Q4ybKQvxgwpJj84h5mb7JE218qTpZQnoA3.json --commitment confirmed 10
-java -jar $ADMIN_CLI initialize -u http://localhost:8899 -v
-java -jar $ADMIN_CLI create-network -u http://localhost:8899 -v
-java -jar $ADMIN_CLI authorize --address DevNMdtQW3Q4ybKQvxgwpJj84h5mb7JE218qTpZQnoA3 --network 0 -u http://localhost:8899 -v
+java -jar $ADMIN_CLI initialize -u http://localhost:8899 -v -k solana-aggregator/notary-program/dev-keys/DevAD5S5AFhTTCmrD8Jg58bDhbZabSzth7Bu6rG4HFYo.json
+java -jar $ADMIN_CLI create-network -u http://localhost:8899 -k solana-aggregator/notary-program/dev-keys/DevAD5S5AFhTTCmrD8Jg58bDhbZabSzth7Bu6rG4HFYo.json
+java -jar $ADMIN_CLI authorize --address DevNMdtQW3Q4ybKQvxgwpJj84h5mb7JE218qTpZQnoA3 --network 0 -u http://localhost:8899 -k solana-aggregator/notary-program/dev-keys/DevAD5S5AFhTTCmrD8Jg58bDhbZabSzth7Bu6rG4HFYo.json
