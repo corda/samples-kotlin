b02e7ba5a6ac5efa7b0b388014042c4ef64d0026
divas.kumar <divas.kumar@r3.com>
2026-06-12 12:03:37 +0530

    ENT-15129: remove nested approch

--- Features/referenceStates-sanctionsBody/workflows/src/main/kotlin/net/corda/samples/referencestates/flows/IssueSanctionsListFlow.kt
+++ Features/referenceStates-sanctionsBody/workflows/src/main/kotlin/net/corda/samples/referencestates/flows/IssueSanctionsListFlow.kt
@@ -18,73 +18,70 @@ import net.corda.core.utilities.ProgressTracker
 import net.corda.core.utilities.ProgressTracker.Step
 
 /**
- * This flow allows the sanctions authority to issue an initial empty sanctions list.
+ * Flow to issue an initial empty sanctions list
  */
-object IssueSanctionsListFlow {
+@InitiatingFlow
+@StartableByRPC
+class IssueSanctionsListFlow : FlowLogic<StateAndRef<SanctionedEntities>> {
 
-    @InitiatingFlow
-    @StartableByRPC
-    class Initiator : FlowLogic<StateAndRef<SanctionedEntities>> {
+    private val notary: Party?
 
-        private val notary: Party?
+    // Constructor without notary (for backward compatibility)
+    constructor() {
+        this.notary = null
+    }
 
-        // Constructor without notary (for backward compatibility)
-        constructor() {
-            this.notary = null
+    // Constructor with notary
+    constructor(notary: Party) {
+        this.notary = notary
+    }
+
+    companion object {
+        object GENERATING_TRANSACTION : Step("Generating Transaction")
+        object SIGNING_TRANSACTION : Step("Signing transaction with our private key.")
+        object FINALISING_TRANSACTION : Step("Recording transaction.") {
+            override fun childProgressTracker() = FinalityFlow.tracker()
         }
 
-        // Constructor with notary
-        constructor(notary: Party) {
-            this.notary = notary
-        }
+        fun tracker() = ProgressTracker(
+            GENERATING_TRANSACTION,
+            SIGNING_TRANSACTION,
+            FINALISING_TRANSACTION
+        )
+    }
 
-        companion object {
-            object GENERATING_TRANSACTION : Step("Generating Transaction")
-            object SIGNING_TRANSACTION : Step("Signing transaction with our private key.")
-            object FINALISING_TRANSACTION : Step("Recording transaction.") {
-                override fun childProgressTracker() = FinalityFlow.tracker()
-            }
+    override val progressTracker = tracker()
 
-            fun tracker() = ProgressTracker(
-                GENERATING_TRANSACTION,
-                SIGNING_TRANSACTION,
-                FINALISING_TRANSACTION
+    @Suspendable
+    override fun call(): StateAndRef<SanctionedEntities> {
+        // Determine notary to use
+        val selectedNotary = notary ?: serviceHub.networkMapCache.notaryIdentities.firstOrNull()
+        ?: throw FlowException("No notary available and none specified.")
+
+        logger.info("Using notary: ${selectedNotary.name}")
+
+        progressTracker.currentStep = GENERATING_TRANSACTION
+        val state = SanctionedEntities(emptyList(), serviceHub.myInfo.legalIdentities.first())
+        val txCommand = Command(
+            SanctionedEntitiesContract.Commands.Create,
+            serviceHub.myInfo.legalIdentities.first().owningKey
+        )
+        val txBuilder = TransactionBuilder(selectedNotary)
+            .addOutputState(state, SanctionedEntitiesContract.SANCTIONS_CONTRACT_ID)
+            .addCommand(txCommand)
+
+        txBuilder.verify(serviceHub)
+
+        progressTracker.currentStep = SIGNING_TRANSACTION
+        val partSignedTx = serviceHub.signInitialTransaction(txBuilder)
+
+        progressTracker.currentStep = FINALISING_TRANSACTION
+        return subFlow(
+            FinalityFlow(
+                partSignedTx,
+                sessions = emptyList(),
+                progressTracker = FINALISING_TRANSACTION.childProgressTracker()
             )
-        }
-
-        override val progressTracker = tracker()
-
-        @Suspendable
-        override fun call(): StateAndRef<SanctionedEntities> {
-            // Determine notary to use
-            val selectedNotary = notary ?: serviceHub.networkMapCache.notaryIdentities.firstOrNull()
-            ?: throw FlowException("No notary available and none specified.")
-
-            logger.info("Using notary: ${selectedNotary.name}")
-
-            progressTracker.currentStep = GENERATING_TRANSACTION
-            val state = SanctionedEntities(emptyList(), serviceHub.myInfo.legalIdentities.first())
-            val txCommand = Command(
-                SanctionedEntitiesContract.Commands.Create,
-                serviceHub.myInfo.legalIdentities.first().owningKey
-            )
-            val txBuilder = TransactionBuilder(selectedNotary)
-                .addOutputState(state, SanctionedEntitiesContract.SANCTIONS_CONTRACT_ID)
-                .addCommand(txCommand)
-
-            txBuilder.verify(serviceHub)
-
-            progressTracker.currentStep = SIGNING_TRANSACTION
-            val partSignedTx = serviceHub.signInitialTransaction(txBuilder)
-
-            progressTracker.currentStep = FINALISING_TRANSACTION
-            return subFlow(
-                FinalityFlow(
-                    partSignedTx,
-                    sessions = emptyList(),
-                    progressTracker = FINALISING_TRANSACTION.childProgressTracker()
-                )
-            ).tx.outRefsOfType(SanctionedEntities::class.java).single()
-        }
+        ).tx.outRefsOfType(SanctionedEntities::class.java).single()
     }
 }
