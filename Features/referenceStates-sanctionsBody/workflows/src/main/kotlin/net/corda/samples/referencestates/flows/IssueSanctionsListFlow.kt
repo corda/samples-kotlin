f6748716903998356ca3fdea86f08dcd91bb9d80
divas.kumar <divas.kumar@r3.com>
2026-06-12 11:43:24 +0530

    ENT-15129: Added JvmOverloads with constructor

--- Features/referenceStates-sanctionsBody/workflows/src/main/kotlin/net/corda/samples/referencestates/flows/IssueSanctionsListFlow.kt
+++ Features/referenceStates-sanctionsBody/workflows/src/main/kotlin/net/corda/samples/referencestates/flows/IssueSanctionsListFlow.kt
@@ -1,3 +1,5 @@
+// File: workflows/src/main/kotlin/net/corda/samples/referencestates/flows/IssueSanctionsListFlow.kt
+
 package net.corda.samples.referencestates.flows
 
 import co.paralleluniverse.fibers.Suspendable
@@ -15,23 +17,21 @@ import net.corda.core.transactions.TransactionBuilder
 import net.corda.core.utilities.ProgressTracker
 import net.corda.core.utilities.ProgressTracker.Step
 
+/**
+ * This flow allows the sanctions authority to issue an initial empty sanctions list.
+ * The sanctions list can later be updated using UpdateSanctionsListFlow.
+ */
 object IssueSanctionsListFlow {
+
     @InitiatingFlow
     @StartableByRPC
-    class Initiator : FlowLogic<StateAndRef<SanctionedEntities>> {
-
-        private val notary: Party?
-
-        // Primary constructor for shell usage
-        constructor(notary: Party) {
-            this.notary = notary
-        }
-
-        // Secondary constructor for backward compatibility
-        constructor() {
-            this.notary = null
-        }
-
+    class Initiator @JvmOverloads constructor(
+        private val notary: Party? = null
+    ) : FlowLogic<StateAndRef<SanctionedEntities>>() {
+        /**
+         * The progress tracker checkpoints each stage of the flow and outputs the specified messages when each
+         * checkpoint is reached in the code. See the 'progressTracker.currentStep' expressions within the call() function.
+         */
         companion object {
             object GENERATING_TRANSACTION : Step("Generating Transaction")
             object SIGNING_TRANSACTION : Step("Signing transaction with our private key.")
@@ -48,6 +48,9 @@ object IssueSanctionsListFlow {
 
         override val progressTracker = tracker()
 
+        /**
+         * The flow logic is encapsulated within the call() method.
+         */
         @Suspendable
         override fun call(): StateAndRef<SanctionedEntities> {
             // Determine notary to use
@@ -56,7 +59,9 @@ object IssueSanctionsListFlow {
 
             logger.info("Using notary: ${selectedNotary.name}")
 
+            // Stage 1.
             progressTracker.currentStep = GENERATING_TRANSACTION
+            // Generate an unsigned transaction.
             val state = SanctionedEntities(emptyList(), serviceHub.myInfo.legalIdentities.first())
             val txCommand = Command(
                 SanctionedEntitiesContract.Commands.Create,
@@ -68,10 +73,14 @@ object IssueSanctionsListFlow {
 
             txBuilder.verify(serviceHub)
 
+            // Stage 3.
             progressTracker.currentStep = SIGNING_TRANSACTION
+            // Sign the transaction.
             val partSignedTx = serviceHub.signInitialTransaction(txBuilder)
 
+            // Stage 5.
             progressTracker.currentStep = FINALISING_TRANSACTION
+            // Notarise and record the transaction in both parties' vaults.
             return subFlow(
                 FinalityFlow(
                     partSignedTx,
